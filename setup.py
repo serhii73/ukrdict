9804d4a264685f843b1703ad7d58fc8497d4fb33
serhii <aserhii@protonmail.com>
2019-07-19 13:13:07 +0300

    Remove not need info
    Remove requirements txt
    Remove not need info from setup

--- setup.py
+++ setup.py
@@ -1,19 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
-
-setup(
-    name="ukrdict",
-    description="Library for searching ukrainian words meaning.",
-    version="0.1",
-    author_email="aserhii@protonmail.com",
-    author="serhii73",
-    license="MIT License",
-    url="https://github.com/serhii73/python_sum_in_ua_api",
-    packages=find_packages(),
-    install_requires=requirements,
-    entry_points={"console_scripts": ["ukrdict = ukrdict.main:main"]},
-)
+setup(entry_points={"console_scripts": ["ukrdict = ukrdict.main:main"]})
