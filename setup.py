f725fe93eaa62a33df3e7120b56420945c5d0c85
serhii <aserhii@protonmail.com>
2019-07-19 09:23:23 +0300

    Update setuptools version

--- setup.py
+++ setup.py
@@ -2,16 +2,11 @@
 # -*- coding: utf-8 -*-
 """Distribution setup module."""
 
-__requires__ = ('setuptools >=41.0.0', )
+__requires__ = ("setuptools >=40.0",)
 # The list of pre-requisite requirements, needed to run this module
 
 from setuptools import setup
 
-setup_params = {
-    'use_scm_version': True,
-    'setup_requires': [
-        'setuptools_scm>=3.3',
-    ]
-}
+setup_params = {"use_scm_version": True, "setup_requires": ["setuptools_scm>=3.3"]}
 
-__name__ == '__main__' and setup(**setup_params)
+__name__ == "__main__" and setup(**setup_params)
