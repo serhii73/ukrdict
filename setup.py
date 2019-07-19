f4d17d3f3f4fc206d0434f8b664dc6bbdcd812a2
serhii <aserhii@protonmail.com>
2019-07-19 08:37:03 +0300

    Add setup files and update travis config for pypi

--- setup.py
+++ setup.py
@@ -1,19 +1,17 @@
+#! /usr/bin/env python
 # -*- coding: utf-8 -*-
+"""Distribution setup module."""
 
-from setuptools import find_packages, setup
+__requires__ = ('setuptools >=41.0.0', )
+# The list of pre-requisite requirements, needed to run this module
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
+from setuptools import setup
 
-setup(
-    name="ukrdict",
-    description="Library for searching ukrainian words meaning.",
-    version="0.1",
-    author_email="aserhii@protonmail.com",
-    author="serhii73",
-    license="MIT License",
-    url="https://github.com/serhii73/python_sum_in_ua_api",
-    packages=find_packages(),
-    install_requires=requirements,
-    entry_points={"console_scripts": ["ukrdict = ukrdict.main:main"]},
-)
+setup_params = {
+    'use_scm_version': True,
+    'setup_requires': [
+        'setuptools_scm>=3.3',
+    ]
+}
+
+__name__ == '__main__' and setup(**setup_params)
